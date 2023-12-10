394bcce79b8db975b2e915e5426c675dfa2e06a9
ynori123 <norisama1159@gmail.com>
2023-12-10 18:53:39 +0900

    missesテーブルの定義

---
+++ backend/src/models/misses.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Double, Integer, String, ForeignKey, UUID, DateTime, Text
+from sqlalchemy.orm import relationship
+from src.database import Base
+import uuid
+
+
+class Miss(Base):
+    __tablename__ = "misses"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, autoincrement=True, default=uuid.uuid4)
+    user_id = Column(Integer, ForeignKey("users.id"), required=True)
+    subject_id = Column(Integer, ForeignKey("subjects.id"), required=True)
+    name = Column(String(128), required=True, index=True)
+    datetime = Column(DateTime, required=True, index=True)
+    time = Column(Double, required=True)
+    memo = Column(Text)
+    
+    subject_id = relationship("Subject", backref="misses")
+    user_id = relationship("User", backref="misses")    

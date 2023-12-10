42343fdc79c4b19ac48e41bc13c5dc1a43d241c4
ynori123 <norisama1159@gmail.com>
2023-12-10 16:10:59 +0900

    CORS設定の変更

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],  # 許可するオリジンを指定
     allow_credentials=True,
-    allow_methods=["GET", "POST"],  # 許可するHTTPメソッドを指定（"*"はすべてのメソッドを許可）
+    allow_methods=["GET", "POST", "PUT", "DELETE"], 
     allow_headers=["*"],  # 許可するヘッダーを指定（"*"はすべてのヘッダーを許可）
 )
 
@@ -47,9 +47,9 @@ misses = [
     {
         "id": 1,
         "miss" : [
-            {"id": "06c0b353-4782-4c4a-852a-92cd9b56ae2c", "subject": "情報工学実験", "date": "2021-06-01", "time": 1, "memo": "バス遅延"},
+            {"id": "06c0b353-4782-4c4a-852a-92cd9b56ae2c", "subject": "情報工学実験", "date": "2021-06-01", "time": 4, "memo": "バス遅延"},
             {"id": "94931ff6-a794-47e4-b08b-cd9756ac25b4","subject": "英語", "date": "2021-06-02", "time": 2, "memo": "寝坊"},
-            {"id": "990ed763-041a-4fa5-bd89-8217d8d6e072","subject": "数学α", "date": "2021-06-03", "time": 3, "memo": ""}
+            {"id": "990ed763-041a-4fa5-bd89-8217d8d6e072","subject": "数学α", "date": "2021-06-03", "time": 2, "memo": ""}
         ],
         "user_id": 1
     },
@@ -154,12 +154,58 @@ async def register(register: RegisterRequest, token: str):
             "code" : 1
         }
 
+@app.get("/miss/{id}")
+def get_miss(id: str, token: str):
+    if token == "85e1150b-b0a2-40cf-b854-bb7b04016fd9":
+        for m in misses[0]["miss"]:
+            if m["id"] == id:
+                return {
+                "code" : 0,
+                "miss" : m
+                }
+        return {
+        "code" : 1,
+        "miss" : None
+        }
+    else:
+        return{
+            "code" : 1,
+            "miss" : None
+        }
+
+class EditRequest(BaseModel):
+    time: int
+    date: str
+    memo: str
+
+@app.put("/miss/{id}")
+def edit_miss(id: str, edit: EditRequest, token: str):
+    if token == "85e1150b-b0a2-40cf-b854-bb7b04016fd9":
+        for m in misses[0]["miss"]:
+            if m["id"] == id:
+                m["time"] = edit.time
+                m["date"] = edit.date
+                m["memo"] = edit.memo
+                return {
+                "code" : 0,
+                "miss" : m
+                }
+        return {
+        "code" : 1,
+        "miss" : None
+        }
+    else:
+        return{
+            "code" : 1,
+            "miss" : None
+        }
+
 @app.get("/misses/all")
 def get_misses_all(token: str):
     if token == "85e1150b-b0a2-40cf-b854-bb7b04016fd9":
         data = []
         for m in misses[0]["miss"]:
-            data.append({"subject": m["subject"], "time": m["time"], "date":m["date"], "memo":m["memo"]})
+            data.append({"id":m["id"],"subject": m["subject"], "time": m["time"], "date":m["date"], "memo":m["memo"]})
         return {
         "code" : 0,
         "misses" : data

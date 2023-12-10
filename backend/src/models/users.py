7112b3d92b8a3c5e9594cc5dada314b34cb7ce2b
ynori123 <norisama1159@gmail.com>
2023-12-10 18:47:36 +0900

    user table

---
+++ backend/src/models/users.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from src.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    class_id = Column(Integer, ForeignKey("classes.id"), required=True)
+    name = Column(String(128), required=True, index=True)
+    email = Column(String(128), unique=True, required=True, index=True)
+    hashed_password = Column(String(256), required=True)
+    is_available = Column(Integer, default=1)
+    
+    tokens = relationship("Token", backref="users")
+    misses = relationship("Miss", backref="users")
+    classes = relationship("Class", backref="users")
+    

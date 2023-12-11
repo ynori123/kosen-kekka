23ca3e32982dd32439f9cf64dc5ede4351bbec4f
ynori123 <norisama1159@gmail.com>
2023-12-11 13:51:43 +0900

    fix pk

--- backend/src/models/subjects.py
+++ backend/src/models/subjects.py
@@ -7,7 +7,7 @@ from src.database import Base
 class Subject(Base):
     __tablename__ = "subjects"
 
-    id = Column(Integer(as_uuid=True), primary_key=True, index=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     class_id = Column(Integer, ForeignKey("classes.id"), required=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     

a2c4f217e03d5d396df0c65897d07684a23ed215
ynori123 <norisama1159@gmail.com>
2023-12-11 13:45:07 +0900

    remove autoincrement

--- backend/src/models/misses.py
+++ backend/src/models/misses.py
@@ -7,7 +7,7 @@ import uuid
 class Miss(Base):
     __tablename__ = "misses"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, index=True, autoincrement=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     user_id = Column(Integer, ForeignKey("users.id"), required=True)
     subject_id = Column(Integer, ForeignKey("subjects.id"), required=True)
     name = Column(String(128), required=True, index=True)
